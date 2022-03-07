4ab4c76f71aaa98f1cf9aef0d93b2b7104dd3c8f
Baturpcu <batusalkim11@gmail.com>
2022-03-08 01:49:47 +0300

    Deneme Flutter

--- dart new/variables/main.dart
+++ dart new/variables/main.dart
@@ -1,8 +1,8 @@
 void main(){
   CustomerManager customerManager = new CustomerManager();
-  Customer customer = new Customer();
-  customer.name = "Batuhan";
-  customer.lastname = "Salkım";
+  Customer customer = new Customer.namedCons("Batuhan","Salkım",1);
+  customer.name = "Derin";
+  customer.lastname = "AKın";
   customer.id = 1;
 
 
@@ -27,10 +27,15 @@ class Customer {
   late String name;
   late String lastname;
   late int id;
+  Customer.namedCons(String name, String lastname, int id ){
+    this.name = name;
+    this.lastname = lastname;
+    this.id = id;
+    print("Sınıf Oluştu...");
+  }
+
+  Customer(){
+
+  }
 }
 
-class Bilgiler {
-  late String isim;
-  late String soyad;
-  late int numara;
-}
