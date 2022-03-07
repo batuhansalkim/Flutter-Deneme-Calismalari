308d5bc8097c664c44ae77a8a560c87ace51267d
Baturpcu <batusalkim11@gmail.com>
2022-03-08 00:41:40 +0300

    Flutter Deneme Çalismalari (Class)
    Flutter Öğreniyorum...

---
+++ dart new/variables/main.dart
@@ -0,0 +1,31 @@
+void main(){
+  CustomerManager customerManager = new CustomerManager();
+  Customer customer = new Customer();
+  customer.name = "Batuhan";
+  customer.lastname = "Salkım";
+  customer.id = 1;
+
+
+  customerManager.Save(customer);
+  customerManager.Update(customer);
+  customerManager.Delete(customer);
+}
+
+class CustomerManager {
+  void Save(Customer customer){
+    print("Müşteri Kaydedildi : " + customer.name + customer.lastname);
+  }
+  void Delete(Customer customer){
+    print("Müşteri Silindi: "+customer.name);
+  }
+  void Update(Customer customer){
+    print("Müşteri Güncellendi: "+customer.name);
+  }
+}
+
+class Customer {
+  late String name;
+  late String lastname;
+  late int id;
+  
+}
